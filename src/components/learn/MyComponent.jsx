c6d43337fb0f719ad169f6232bd1c894a7825f5f
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-06-27 23:35:20 +0900

    #31Cach su dung bien so voi JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,13 +4,24 @@ import './style.css';
 // component = html + css + js
 // JSX
 const MyComponent = () => {
+    // const hoidanit = "eric1"; // string
+    // const hoidanit = 25; // number
+    // const hoidanit = true;// boolean
+    // const hoidanit = undefined;
+    // const hoidanit = null;
+    const hoidanit = [1,2,3];
+    // const hoidanit = {
+    //     name: 'hoidanit',
+    //     age: 10
+    // }
     return(
         <>
-
-                <div className=""> eric hhh</div>
+                <div>{console.log("eric")}</div>
+                <div className=""> {JSON.stringify(hoidanit)} hhh</div>
                 <div className='child' style={
                     {borderRadius: "10px"}
-                }>child</div>
+                }
+                >child</div>
 
         </>
     );
