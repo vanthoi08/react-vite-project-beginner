415d786dcf3b59c9cc3713bc950f62f5fbc40568
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-06-28 23:09:23 +0900

    #34 Props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import TodoNew from "./components/todo/TodoNew"
 import "./components/todo/todo.css"
 import reactLogo from './assets/react.svg'
 const App = () => {
-
+  const hoidanit = "Eric";
+  const age = 25;
+  const date ={
+    address: "Ha Noi",
+    country: "Viet Nam"
+  }
+// {key:value}
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-     <TodoData />
+     <TodoData 
+     name={hoidanit}
+     age={age}
+     data={date}
+     />
      <div className="todo-image">
       <img className="logo" src={reactLogo}/>
      </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,20 @@
-const TodoData = () =>{
+const TodoData = ({props}) =>{
+  // props là một object
+  // {
+  //   name: "eric",
+  //   age: 25,
+  //   data: {}
+  // }
+  // cach 1
+  // const {name, age, data} = props;
+  // cach 2:
+  // const name = props.name;
+  // const age = props.age;
+  // const data = props.data;
+console.log(">>> check props: ", props);
     return(
         <div className="todo-data">
+          <div>My name is {name}</div>
         <div>Learning React</div>
         <div>Watching Youtube</div>
       </div>
