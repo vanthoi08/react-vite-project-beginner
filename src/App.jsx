d91f39cf25df55993d1f01b8f621df2d5b6e2910
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-06-30 09:38:48 +0900

    #47 Tao Header/Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoNew from "./components/todo/TodoNew";
 import "./components/todo/todo.css";
 import reactLogo from "./assets/react.svg";
 import { useState } from "react";
+import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
 const App = () => {
   const [todoList, setTodoList] = useState([
     // {id:1, name: "Learning React"},
@@ -21,26 +23,27 @@ const App = () => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
-  const deleteTodo = (id)=>{
-    const newTodo = todoList.filter(item => item.id !== id)
+  const deleteTodo = (id) => {
+    const newTodo = todoList.filter((item) => item.id !== id);
     setTodoList(newTodo);
-  }
+  };
   // {key:value}
   return (
-    <div className="todo-container">
-      <div className="todo-title">Todo List</div>
-      <TodoNew addNewTodo={addNewTodo} />
-{
-        todoList.length > 0 ?
-        <TodoData todoList={todoList}
-                  deleteTodo = {deleteTodo}
-        />
-        :
-        <div className="todo-image">
-           <img className="logo" src={reactLogo} />
+    <>
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoNew addNewTodo={addNewTodo} />
+        {todoList.length > 0 ? (
+          <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+        ) : (
+          <div className="todo-image">
+            <img className="logo" src={reactLogo} />
+          </div>
+        )}
       </div>
-      }
-    </div>
+      <Footer />
+    </>
   );
 };
 
