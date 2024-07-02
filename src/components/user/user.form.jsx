2f60b90f5cf70de07c96f769ac585a1623202ce7
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-07-02 22:12:30 +0900

    #Xu lys loi voi Interceptor

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -17,6 +17,11 @@ const UserForm = () => {
         message: "create user",
         description: "Tạo user thành công",
       });
+    } else {
+      notification.error({
+        message: "Error create user",
+        description: JSON.stringify(res.message)
+      })
     }
   };
   return (
