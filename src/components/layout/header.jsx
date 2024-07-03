263bceda51cff328bb95bea01c0d8c6bcf01c14c
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-07-03 23:12:42 +0900

    #76 Design Modal Update User

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,12 +1,42 @@
-import { Link, NavLink} from 'react-router-dom';
-import './header.css'
+import { Link, NavLink } from "react-router-dom";
+import { Menu } from "antd";
+import {
+  UsergroupAddOutlined,
+  HomeOutlined,
+  AuditOutlined,
+} from "@ant-design/icons";
+import { useState } from "react";
 const Header = () => {
-    return(
-        <ul>
-            <li><NavLink class="active" to="/">Home</NavLink></li>
-            <li><NavLink to="/users">Users</NavLink></li>
-            <li><NavLink to="/books">Books</NavLink></li>
-        </ul>
-    )
-}
+  const [current, setCurrent] = useState("");
+
+  const onClick = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+  const items = [
+    {
+      label: <Link to={"/"}>Home</Link>,
+      key: "home",
+      icon: <HomeOutlined />,
+    },
+    {
+      label: <Link to={"/users"}>Users</Link>,
+      key: "users",
+      icon: <UsergroupAddOutlined />,
+    },
+    {
+      label: <Link to={"/books"}>Books</Link>,
+      key: "products",
+      icon: <AuditOutlined />,
+    },
+  ];
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
+  );
+};
 export default Header;
--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,13 +1,17 @@
-import {Table} from "antd";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { Table } from "antd";
+import UpdateUserModal from "./update.user.modal";
 
 const UserTable = (props) => {
-const {dataUsers} = props;
-
+  const { dataUsers } = props;
 
   const columns = [
     {
       title: "Id",
       dataIndex: "_id",
+      render: (_, record) => {
+        return <a href="#">{record._id}</a>;
+      },
     },
     {
       title: "Full Name",
@@ -17,16 +21,29 @@ const {dataUsers} = props;
       title: "Email",
       dataIndex: "email",
     },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <div style={{ display: "flex", gap: "20px" }}>
+          <EditOutlined style={{ cursor: "pointer", color: "orange" }} />
+          <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
+        </div>
+      ),
+    },
   ];
-
-
 
   console.log(">>> run render 000");
 
-  return <Table 
-      columns={columns} 
-        dataSource={dataUsers}
-        rowKey={"_id"}
-        />;
+  return(
+    <>
+    <Table 
+    columns={columns} 
+    dataSource={dataUsers}
+     rowKey={"_id"} 
+     />
+     <UpdateUserModal />
+     </>
+  )
 };
 export default UserTable;
