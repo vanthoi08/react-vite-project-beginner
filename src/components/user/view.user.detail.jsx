0371494342b1c217248a83990671c1e93d6a676f
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-07-13 12:18:44 +0900

    #82 Hien thi Avatar User

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,4 +1,4 @@
-import { Drawer } from 'antd';
+import { Button, Drawer } from 'antd';
 
 const ViewUserDetail = (props) =>{
     const {
@@ -11,7 +11,9 @@ const ViewUserDetail = (props) =>{
 
     return (
         <>
-          <Drawer title="Chi tiết User" 
+          <Drawer
+          width={"40vw"}
+           title="Chi tiết User" 
           onClose={()=>{
             setDataDetail(null)
             setIsDetailOpen(false)
@@ -27,6 +29,37 @@ const ViewUserDetail = (props) =>{
                 <br />
                 <p>Phone: {dataDetail.phone}</p>
                 <br />
+                <p>Avatar :</p>
+                <br />
+                <div
+                style={{
+                  marginTop:"10px",
+                  height: "100px",
+                  width:"150px",
+                  border: "1px solid #ccc"
+                }}
+                >
+                  <img 
+                  style={{height:"100%", width:"100%",objectFit: "contain"}}
+                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
+                </div>
+                <div>
+                  <label htmlFor='btnUpload'
+                  style={{
+                    display:"block",
+                    width:"fit-content",
+                    marginTop:"15px",
+                    padding: "5px 10px",
+                    background: "orange",
+                    borderRadius:"5px",
+                    cursor:"pointer"
+                  }}
+                  >
+                    Upload Avatar
+                    </label>
+                  <input type='file' hidden id='btnUpload'/>
+                </div>
+                {/* <Button type='primary'>Upload Avatar</Button> */}
             </>
             :
             <>
