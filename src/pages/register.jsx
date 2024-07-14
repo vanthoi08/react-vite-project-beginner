d457791b6efec7dcde5e20470ef50031ff5fb171
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-07-14 23:44:05 +0900

    #89 Su dung Uncontrolled Component cho Resgister

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,84 @@
+import { Button, Input, Form } from "antd";
+const RegisterPage = () => {
+  const [form] = Form.useForm();
 
-const RegisterPage = () =>{
-    return(
-        <div>register page</div>
-    )
-}
+  const onFinish = (values) =>{
+    console.log(values)
+  }
+  return (
+    <Form
+      form={form}
+      layout="vertical"
+        onFinish={onFinish}
+      //   onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <div
+        style={{
+          margin: "50px",
+        }}
+      >
+        <Form.Item
+          label="Full Name"
+          name="fullName"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="Phone number"
+          name="phone"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <div>
+          <Button 
+          onClick={()=> form.submit()}
+           type="primary">
+            Register
+            </Button>
+        </div>
+      </div>
+    </Form>
+  );
+};
 export default RegisterPage;
